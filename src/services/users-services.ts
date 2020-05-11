dcf58b31c8a0a022410fe70260be30776cd4c899
Yurrian Pierre-Boyer <ryanx101@gmail.com>
2020-05-10 21:43:07 -0400

    2nd Commit W/ Folders

---
+++ src/services/users-services.ts
@@ -0,0 +1,12 @@
+export function getAllUsers(): Promise <Person[]> {
+    
+}
+
+export function getUserById(id: number): Person {
+
+}
+    
+export function saveUser(user: any): Person {
+    
+
+}
